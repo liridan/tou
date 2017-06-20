bbafedd8c9930dbb5de5bc485025e79c65505cb9
Your Name <email@example.com>
2017-06-20 17:31:05 +0800

    to

--- Touch/js/search.js
+++ Touch/js/search.js
@@ -20,26 +20,26 @@ $(function(){
 			})
 		}
 	})	
-	var arr="";
+	
 	$("#cancel").click(function(){
 		$("#show").html("");
-	  	$.ajax({
-			type:"get",
-			url:"http://api.search.sina.com.cn/?c=news&q="+$("#txt").val()+"&stime=2016-06-05&etime=2017-06-07&sort=rel&highlight=1&num=10&ie=utf-8&callback",
-			dataType:"jsonp",
-			success:function(data){
-				console.log(data.result.list);
-				for(var i=0;i<data.result.list.length;i++){
-					var n=data.result.list[i].title;
-					var url=data.result.list[i].url;
-					arr+="<li><a href="+url+">"+n+"</a></li>";
-					
-					$("#show").html(arr);
+		var arr="";
+			$.ajax({
+				type:"get",
+				url:"http://api.search.sina.com.cn/?c=news&q="+$("#txt").val()+"&stime=2016-06-05&etime=2017-06-07&sort=rel&highlight=1&num=10&ie=utf-8&callback",
+				dataType:"jsonp",
+				success:function(data){
+					console.log(data.result.list);
+					for(var i=0;i<data.result.list.length;i++){
+						var n=data.result.list[i].title;
+						var url=data.result.list[i].url;
+						arr+="<li><a href="+url+">"+n+"</a></li>";
+						
+						$("#show").html(arr);
+					}
 				}
-				
-				
-			}
-		}); 
+			}); 	
+	  	
 	})
 })
 
