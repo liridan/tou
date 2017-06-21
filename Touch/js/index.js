cbe84ba3ae9684e2f64664d4e29ec78356ed8493
Your Name <email@example.com>
2017-06-21 10:56:31 +0800

    to

--- Touch/js/index.js
+++ Touch/js/index.js
@@ -1,11 +1,6 @@
 $(function(){
-	//首页上面的点击效果
-	$("#topline").on("tap",function(){
-		$(".home-con").animate({marginLeft:0});
-		$(this).addClass("active").siblings().removeClass("active");
+	$("#footlist li").on("tap",function(){
+		$(this).addClass("active").siblings().removeClass("active")
 	});
-	$("#subscribe").on("tap",function(){
-		$(".home-con").animate({marginLeft:"-16rem"});
-		$(this).addClass("active").siblings().removeClass("active");
-	})
+	
 })
